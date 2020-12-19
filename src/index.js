2602290457e7fc29226aa9dc7a549111c620831e
OleksiiMaistriuk <65893898+OleksiiMaistriuk@users.noreply.github.com>
2020-12-19 21:09:06 +0100

    закончил

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 import './styles.css';
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+  bodyColor: document.querySelector('body'),
+  startButton: document.querySelector('.start'),
+  stopButton: document.querySelector('.stop'),
+};
+
+refs.startButton.addEventListener('click', start);
+refs.stopButton.addEventListener('click', stop);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function changeBackground(color) {
+  refs.bodyColor.style.backgroundColor = color;
+}
+
+let intervalColorChange = undefined;
+
+function start() {
+  intervalColorChange = setInterval(randomNumber => {
+    randomNumber = randomIntegerFromInterval(0, 5);
+    changeBackground(colors[randomNumber]);
+  }, 1000);
+  refs.stopButton.removeAttribute('disabled');
+  refs.startButton.setAttribute('disabled', true);
+}
+
+function stop() {
+  clearInterval(intervalColorChange);
+  refs.startButton.removeAttribute('disabled');
+  refs.stopButton.setAttribute('disabled', true);
+}
